2c1a44b6806c490d442b6ef68457b55fdf5b34d7
William Gregory <mail@wmgregory.co.uk>
2016-09-03 13:07:56 +0100

    Untidy list showing on the screen for now

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,16 @@
         // setup transport coach stations
         var transport = new Transport();
         var coachStations = transport.CoachStations();
+        var coachStationList = coachStations.findByPostcode('WC1E 7BL', 3);
 
-        console.log(coachStations.findByPostcode('WC1E 7BL', 3));
+        // clear the existing list
+        $('.transport__coachstations .transport__coachstations-list li').remove();
+
+        $.each(coachStationList, function(index, coachStation) {
+          $('.transport__coachstations .transport__coachstations-list').append('<li><a class="transport__coachstations-list-item list-group-item">'
+            + coachStation.name
+            + '</a></li>');
+        });
     });
 
 })();
